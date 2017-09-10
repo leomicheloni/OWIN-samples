f52e1e2fbdc8e551581c4537d875406741cf1137
Leonardo Micheloni <leonardo.micheloni@tokiota.com>
2017-09-11 00:19:24 +0200

    initial commit

---
+++ Empty WebAPI/InMemoryServer/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace InMemoryServer.controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Test()
+        {
+            return Json("hola");
+        }
+    }
+}
---
+++ InMemoryServer/InMemoryServer/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(InMemoryServer.Startup))]
+namespace InMemoryServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            var config = new HttpConfiguration();
+            
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Test" }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
